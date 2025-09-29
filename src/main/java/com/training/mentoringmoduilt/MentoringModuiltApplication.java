7adf053e1561ffa52f9fec7c8f7b4578bc53e8ae
Tóth András <sba.andras.toth@gmail.com>
2025-09-29 13:20:38 +0200

    Initial commit

---
+++ src/main/java/com/training/mentoringmoduilt/MentoringModuiltApplication.java
@@ -0,0 +1,13 @@
+package com.training.mentoringmoduilt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringModuiltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoringModuiltApplication.class, args);
+    }
+
+}
