d5872319f2ed44cd9e396799885a8e0df1ef042b
Tóth András <sba.andras.toth@gmail.com>
2025-09-30 09:04:56 +0200

    modulit -> modulith typo fix

--- src/main/java/training/mentoringmodulith/MentoringModuiltApplication.java
+++ src/main/java/training/mentoringmodulith/MentoringModuiltApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MentoringModuiltApplication {
+public class MentoringModuilthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MentoringModuiltApplication.class, args);
+        SpringApplication.run(MentoringModuilthApplication.class, args);
     }
 
 }
